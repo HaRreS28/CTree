679448050c5bb71203e9f4b7417d02410d6deb0d
Marcin <marcinmoch757@gmail.com>
2022-11-25 18:29:39 +0100

    CNodeStatic as interior class

--- Static/CNodeStatic.cpp
+++ Static/CNodeStatic.cpp
@@ -1,68 +1,76 @@
 #include "CNodeStatic.h"
 
-void CNodeStatic::vAddNewChild() {
+//void CNodeStatic::vAddNewChild() {
+//    CNodeStatic cNodeStatic;
+//    cNodeStatic.pc_parent_node = this;
+//    v_children.push_back(cNodeStatic);
+//}
+//
+//CNodeStatic *CNodeStatic::pcGetChild(int iChildOffset) {
+//    if (iChildOffset >= 0 && iChildOffset < iGetChildrenNumber()) {
+//        return &v_children.at(iChildOffset);
+//    } else return NULL;
+//}
+//
+//
+//void CNodeStatic::vPrintAllBelow() {
+//    vPrint();
+//    for (int i = 0; i < v_children.size(); i++) {
+//        pcGetChild(i)->vPrintAllBelow();
+//    }
+//}
+//
+//void CNodeStatic::vPrintUp() {
+//    vPrint();
+//    if (pc_parent_node != NULL)
+//        pc_parent_node->vPrintUp();
+//}
+//
+//CNodeStatic::~CNodeStatic() {
+////    cout<<"delete: ";
+////    vPrint();
+//}
+//
+//CNodeStatic *CNodeStatic::pcGetParent() {
+//    return pc_parent_node;
+//}
+//
+//
+//CNodeStatic *CNodeStatic::pcGetRoot(CNodeStatic *cNodeStatic) {
+//    if (cNodeStatic->pc_parent_node != NULL) {
+//        pcGetRoot(cNodeStatic->pc_parent_node);
+//    }
+//    return cNodeStatic;
+//}
+//
+//void CNodeStatic::vAddNewChild(CNodeStatic &cNodeStatic) {
+//    cNodeStatic.pc_parent_node = this;
+//    v_children.push_back(cNodeStatic);
+//}
+//
+//int CNodeStatic::iGetChildPosition(CNodeStatic &cNodeStatic) {
+//    for (int i = 0; i < v_children.size(); i++) {
+//        if (&v_children.at(i) == &cNodeStatic)
+//            return i;
+//    }
+//    return -1;
+//}
+//
+//bool CNodeStatic::bRemoveChild(CNodeStatic &cNodeStatic) {
+//    int position = iGetChildPosition(cNodeStatic);
+//    if (position != -1) {
+//        v_children.erase(v_children.begin() + position);
+//        return true;
+//    }
+//    return false;
+//}
+
+//interior CNODESTATIC
+
+void CTreeStatic::CNodeStatic::vAddNewChild() {
     CNodeStatic cNodeStatic;
     cNodeStatic.pc_parent_node = this;
     v_children.push_back(cNodeStatic);
-}
-
-CNodeStatic *CNodeStatic::pcGetChild(int iChildOffset) {
-    if (iChildOffset >= 0 && iChildOffset < iGetChildrenNumber()) {
-        return &v_children.at(iChildOffset);
-    } else return NULL;
-}
-
-
-void CNodeStatic::vPrintAllBelow() {
-    vPrint();
-    for (int i = 0; i < v_children.size(); i++) {
-        pcGetChild(i)->vPrintAllBelow();
-    }
-}
-
-void CNodeStatic::vPrintUp() {
-    vPrint();
-    if (pc_parent_node != NULL)
-        pc_parent_node->vPrintUp();
-}
-
-CNodeStatic::~CNodeStatic() {
-//    cout<<"delete: ";
-//    vPrint();
-}
-
-CNodeStatic *CNodeStatic::pcGetParent() {
-    return pc_parent_node;
-}
-
-
-CNodeStatic *CNodeStatic::pcGetRoot(CNodeStatic *cNodeStatic) {
-    if (cNodeStatic->pc_parent_node != NULL) {
-        pcGetRoot(cNodeStatic->pc_parent_node);
-    }
-    return cNodeStatic;
-}
-
-void CNodeStatic::vAddNewChild(CNodeStatic &cNodeStatic) {
-    cNodeStatic.pc_parent_node = this;
-    v_children.push_back(cNodeStatic);
-}
-
-int CNodeStatic::iGetChildPosition(CNodeStatic &cNodeStatic) {
-    for (int i = 0; i < v_children.size(); i++) {
-        if (&v_children.at(i) == &cNodeStatic)
-            return i;
-    }
-    return -1;
-}
-
-bool CNodeStatic::bRemoveChild(CNodeStatic &cNodeStatic) {
-    int position = iGetChildPosition(cNodeStatic);
-    if (position != -1) {
-        v_children.erase(v_children.begin() + position);
-        return true;
-    }
-    return false;
 }
 
 //CLASS CTreeStatic
--- Static/CNodeStatic.h
+++ Static/CNodeStatic.h
@@ -2,50 +2,88 @@
 #include <vector>
 
 using namespace std;
-class CTreeStatic;
-
-class CNodeStatic {
-public:
-    CNodeStatic() {
-        i_val = 0;
-        pc_parent_node = NULL;
-    };
-
-    ~CNodeStatic();
-
-    void vSetValue(int iNewVal) { i_val = iNewVal; };
-
-    int iGetChildrenNumber() { return (v_children.size()); };
-
-    void vAddNewChild();
-
-    void vAddNewChild(CNodeStatic &cNodeStatic);
-
-    int iGetChildPosition(CNodeStatic &cNodeStatic);
-
-    bool bRemoveChild(CNodeStatic &cNodeStatic);
-
-    CNodeStatic *pcGetChild(int iChildOffset);
-
-    CNodeStatic *pcGetParent();
-
-    CNodeStatic *pcGetRoot(CNodeStatic* cNodeStatic);
-
-    void vPrint() { cout << " " << i_val; };
-
-    void vPrintAllBelow();
-
-    void vPrintUp();
-
-private:
-    vector<CNodeStatic> v_children;
-    CNodeStatic *pc_parent_node;
-    int i_val;
-};//class CNodeStatic
+//class CTreeStatic;
+//
+//class CNodeStatic {
+//public:
+//    CNodeStatic() {
+//        i_val = 0;
+//        pc_parent_node = NULL;
+//    };
+//
+//    ~CNodeStatic();
+//
+//    void vSetValue(int iNewVal) { i_val = iNewVal; };
+//
+//    int iGetChildrenNumber() { return (v_children.size()); };
+//
+//    void vAddNewChild();
+//
+//    void vAddNewChild(CNodeStatic &cNodeStatic);
+//
+//    int iGetChildPosition(CNodeStatic &cNodeStatic);
+//
+//    bool bRemoveChild(CNodeStatic &cNodeStatic);
+//
+//    CNodeStatic *pcGetChild(int iChildOffset);
+//
+//    CNodeStatic *pcGetParent();
+//
+//    CNodeStatic *pcGetRoot(CNodeStatic* cNodeStatic);
+//
+//    void vPrint() { cout << " " << i_val; };
+//
+//    void vPrintAllBelow();
+//
+//    void vPrintUp();
+//
+//private:
+//    vector<CNodeStatic> v_children;
+//    CNodeStatic *pc_parent_node;
+//    int i_val;
+//};//class CNodeStatic
 
 
 class CTreeStatic {
 public:
+    class CNodeStatic {
+    public:
+        CNodeStatic() {
+            i_val = 0;
+            pc_parent_node = NULL;
+        };
+
+        ~CNodeStatic();
+
+        void vSetValue(int iNewVal) { i_val = iNewVal; };
+
+        int iGetChildrenNumber() { return (v_children.size()); };
+
+        void vAddNewChild();
+
+        void vAddNewChild(CNodeStatic &cNodeStatic);
+
+        int iGetChildPosition(CNodeStatic &cNodeStatic);
+
+        bool bRemoveChild(CNodeStatic &cNodeStatic);
+
+        CNodeStatic *pcGetChild(int iChildOffset);
+
+        CNodeStatic *pcGetParent();
+
+        CNodeStatic *pcGetRoot(CNodeStatic* cNodeStatic);
+
+        void vPrint() { cout << " " << i_val; };
+
+        void vPrintAllBelow();
+
+        void vPrintUp();
+
+    private:
+        vector<CNodeStatic> v_children;
+        CNodeStatic *pc_parent_node;
+        int i_val;
+    };//class CNodeStatic
 
     CTreeStatic();
 
